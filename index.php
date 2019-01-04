283865dd8b628473626dabbed3e847af069dce10
Serrjik <uac.sarge@gmail.com>
2019-01-04 17:06:07 +0300

    queries for fetching data lesson

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 	// mysqli_connect(server, user, pass, db-name);
-	mysqli_connect('localhost', 'root', '', 'mini-site');
+	$link = mysqli_connect('localhost', 'root', '', 'mini-site');
 
 	// Возвращает ошибку в случае если не получилось соединиться с базой данных
 	// mysqli_connect_error();
@@ -15,6 +15,37 @@
 	if ( mysqli_connect_error() ) {
 		// Полностью блокирует выполнение скрипта
 		die("Ошибка подключения к базе данных.");
+	}
+
+	// $query = "INSERT INTO `users` (`email`, `password`) VALUES ('joker@mail.ru', '777')";
+
+	// LIMIT 1 - ограничение изменений на изменения только в 1 ряду
+
+	// $query = "UPDATE `users` SET `email` = 'joker@hotmail.org' WHERE `id` = 3 LIMIT 1";
+	
+	// $query = "UPDATE `users` SET `password` = '654321' WHERE `email` = 'joker@hotmail.org' LIMIT 1";
+
+	// $query = "SELECT * FROM `users`";
+	// $query = "SELECT * FROM `users` WHERE `id` = 1";
+	// $query = "SELECT * FROM `users` WHERE `email` = 'joker@hotmail.org'";
+
+	// LIKE - выбирает те записи, которые выглядят как заданная, т.е. не строго равные
+	// $query = "SELECT * FROM `users` WHERE `email` LIKE 'joker@hotmail.org'";
+
+	// % в данном запросе означает любое количество любых символов
+	// $query = "SELECT * FROM `users` WHERE `email` LIKE '%hotmail.com'";
+	// $query = "SELECT * FROM `users` WHERE `email` LIKE '%man%'";
+	// $query = "SELECT * FROM `users` WHERE `id` > 5";
+	// $query = "SELECT * FROM `users` WHERE `id` >= 5";
+	$query = "SELECT * FROM `users` WHERE `id` >= 2 AND `email` LIKE '%hotmail.com'";
+
+	if ( $result = mysqli_query($link, $query) ) {
+		
+		while ( $row = mysqli_fetch_array($result) ) {
+			echo "<pre>";
+			print_r($row);
+			echo "</pre>";
+		}
 	}
 
 ?>
